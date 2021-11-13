0e0d004829ec1e6984a2f231bd77886274dad48b
Dmitry Lichmanov <dimlichmanov@gmail.com>
2021-11-13 19:00:06 +0300

    extra msg for readline removed

--- 07_Environmental/libs/newline_read.c
+++ 07_Environmental/libs/newline_read.c
@@ -3,7 +3,7 @@
 #include "readline/readline.h"
 
 size_t newline(char ** line, size_t* length, FILE* file) {
-    *line = readline("-");
+    *line = readline("");
     if (*line == NULL) {
         return -1;
     }
